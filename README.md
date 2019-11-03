6c773b29718300862f87be0854c2c95d4a483e71
afya <wizyty@afya.pl>
2019-11-03 19:16:50 +0100

    wake-up and step function

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ This node was created for my children.
 I realized that they like falling asleep under the light. That's why I thought about automating the process of turning off the light. At the set time Z the initial value X (here the light) is reduced to the set value Y (usually 0).
 The plugin itself, however, does not specifically manage the light, and returns the value "value" in "msg." and this value should be used.
 
+Important! from version 0.0.2 it is possible to count up (and thus slow wake up with light), as well as to set a change step - more in change.log
+now you can also beat the thread
+
 PL
 -----
 Ten węzeł został stworzony z myślą o moich dzieciach.
 Zdałem sobie sprawę, że lubią zasypiać przy zapalonym świetle. Dlatego pomyślałem, żeby zautomatyzować proces wygaszania światła. W zadanym czasie Z wartość początkowa X (tutaj światła) zostaje obniżona do zadanej Y (najczęściej 0).
 Sam plugin nie zarządza jednak konkretnie światłem, a zwraca wartość "value" w "msg." i właśnie tę wartość należy wykorzystać.
+
+Ważne! od wersji 0.0.2 możliwe jest odliczanie w górę (a więc powolne budzenie światłem), a także ustalenie kroku zmiany - więcej w change.log
+teraz można także wątek ubić
--- afya-lullaby/afya-lullaby.js
+++ afya-lullaby/afya-lullaby.js
@@ -22,8 +22,29 @@ module.exports = function(RED) {
       var myLullaby = node.context().flow.get(node.variableName) || {};
 
       */
+      // kill message
+      if ('payload' in msg) {
+        if (String(msg['payload']).toLowerCase() == "kill" || String(msg['payload']).toLowerCase() == "stop") {
+          if ('timeObject' in node) {
+            deleteInterval(node);
+            this.status({
+              fill: "red",
+              shape: "dot",
+              text: "state: killed...  <" + timeConvert(Date.now()) + ">"
+            });
 
-      // exit, if timeObject - also Loop exists
+          } else {
+            this.status({
+              fill: "yellow",
+              shape: "dot",
+              text: "state: tried to kill...but the node stopped  <" + timeConvert(Date.now()) + ">"
+            });
+          }
+          return;
+        }
+      }
+
+      // exit, if started angin : i have timeObject - also Loop exists
       if (node.timeObject !== undefined) {
         return;
       }
@@ -34,6 +55,19 @@ module.exports = function(RED) {
       // this variable changes the state - true/false to choose box or ring as node state
       if (myLullaby.boxOrRing === undefined) {
         myLullaby.boxOrRing = false;
+      }
+
+      //my step up or step down
+      if (myLullaby.step === undefined) {
+        myLullaby.step = Math.round(Math.abs(config.step));
+      }
+      if (myLullaby.step == 0) {
+        this.status({
+          fill: "red",
+          shape: "dot",
+          text: "state: config error - step down or up value is 0 !  <" + timeConvert(Date.now()) + ">"
+        });
+        return;
       }
 
       // startTime, timestamp, not used in all
@@ -51,14 +85,31 @@ module.exports = function(RED) {
         myLullaby.stopTime = myLullaby.startTime + myLullaby.dimmingTime;
       }
 
-      // dimming from startValue
+      // dimming from startValue - positive int only
       if (myLullaby.startValue === undefined) {
-        myLullaby.startValue = config.startValue;
+        myLullaby.startValue = Math.round(Math.abs(config.startValue));
       }
-      // dimming to stopValue
+      // dimming to stopValue - positive int only
       if (myLullaby.stopValue === undefined) {
-        myLullaby.stopValue = config.stopValue;
+        myLullaby.stopValue = Math.round(Math.abs(config.stopValue / 1));
       }
+
+      // check if i have something to do...
+      if (myLullaby.startValue == myLullaby.stopValue) {
+        this.status({
+          fill: "red",
+          shape: "dot",
+          text: "state: config error - startValue = stopValue  <" + timeConvert(Date.now()) + ">"
+        });
+        return;
+      }
+      if (myLullaby.startValue > myLullaby.stopValue) {
+        myLullaby.direction = true; // true = direction down
+      } else {
+        myLullaby.directtion = false; // false = direction up
+      }
+
+
       // my current dimm value is currValue
       if (myLullaby.currValue === undefined) {
         myLullaby.currValue = myLullaby.startValue;
@@ -72,9 +123,14 @@ module.exports = function(RED) {
         myLullaby.dimmingDifference = Math.abs(myLullaby.startValue - myLullaby.stopValue);
       }
 
-      // every how many seconds change the setting value = dimmingTime/dimmingDiference
+      // every how many seconds change the setting value = dimmingTime/ (dimmingDiference/step)
+      var steps = Math.round(myLullaby.dimmingDifference / myLullaby.step);
+      if (steps == 0) {
+        steps = 1;
+      }
+
       if (myLullaby.recallTime === undefined) {
-        myLullaby.recallTime = Math.round(myLullaby.dimmingTime / myLullaby.dimmingDifference);
+        myLullaby.recallTime = Math.round(myLullaby.dimmingTime / steps);
       }
 
       // assign my Lullaby
@@ -93,7 +149,9 @@ module.exports = function(RED) {
       msg = {
         payload: {
           value: myLullaby.currValue,
-          timestamp: Date.now()
+          timestamp: Date.now(),
+          direction: (myLullaby.direction ? "down" : "up"),
+          state: "counting"
         }
       }
       node.timeObject = setInterval(timeLoop, node.myLullaby.recallTime, node);
@@ -114,7 +172,11 @@ module.exports = function(RED) {
   */
   function timeLoop(childNode) {
     var myLullaby = childNode.myLullaby; //childNode.context().flow.get(variableName) || {};
-    myLullaby.currValue = myLullaby.currValue - 1;
+    if (myLullaby.direction) { //direction down
+      myLullaby.currValue = myLullaby.currValue - myLullaby.step;
+    } else { //direction up
+      myLullaby.currValue = myLullaby.currValue + myLullaby.step;
+    }
     myLullaby.boxOrRing = !myLullaby.boxOrRing;
     //childNode.context().flow.set(variableName, myLullaby);
 
@@ -128,17 +190,29 @@ module.exports = function(RED) {
     var msg = {
       payload: {
         value: myLullaby.currValue,
-        timestamp: Date.now()
+        timestamp: Date.now(),
+        direction: (myLullaby.direction ? "down" : "up"),
+        state: "counting"
       }
     }
 
+    /**
+    if we count down and 'currValue' is less than or equal to stopValue
+    or
+    if we count up and the 'currValue' value is at least equal to or greater than the stopValue value
+    or
+    if the time of planned blanking (increase) exceeded the blanking (increase) time + 1 step increase period 1
 
-    if (myLullaby.currValue <= myLullaby.stopValue) {
+    -> counting must be completed
+    */
+    if ((myLullaby.currValue <= myLullaby.stopValue && myLullaby.direction) || (myLullaby.currValue >= myLullaby.stopValue && !myLullaby.direction) || myLullaby.stopTime + myLullaby.recallTime < Date.now()) {
       //ovverride message - currValue sholudn't < stopValue
       msg = {
         payload: {
           value: myLullaby.stopValue,
-          timestamp: Date.now()
+          timestamp: Date.now(),
+          direction: (myLullaby.direction ? "down" : "up"),
+          state: "end"
         }
       }
 
